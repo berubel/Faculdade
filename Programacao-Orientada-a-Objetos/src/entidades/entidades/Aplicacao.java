e26ea9605703896cbb8db42c60975d6ab2b5232a
Gabriele Cardoso <gabrielecardosov@gmail.com>
2021-10-24 11:25:50 -0300

    Third commit: Adicionado case de imposto total arrecadado

--- Programacao-Orientada-a-Objetos/src/entidades/entidades/Aplicacao.java
+++ Programacao-Orientada-a-Objetos/src/entidades/entidades/Aplicacao.java
@@ -15,9 +15,8 @@ import entidades.Pessoa;
  * Alunos: 2010840 - Gabriele Cardoso das Virgens
  * 		   2011321 - João Cândido de Jesus Neto
  */
- 
 
-public class Aplicacao 
+public class Aplicacao
 {
 	static int N;
 	static int id;
@@ -32,7 +31,7 @@ public class Aplicacao
 			opc = sc.nextInt();
 			if(opc > 2 || opc < 1) 
 			{
-				System.out.println("\n\nOpção Invalida! Digite novamente");	
+				System.out.println("\n\nOpção Inválida! Digite novamente");	
 			}	
 		}while(opc > 2 || opc < 1);	
 		
@@ -43,8 +42,8 @@ public class Aplicacao
 		System.out.println("\n\nContribuinte nº " + id);
 		System.out.println("\nTipo: " + listaPF.get(id).tipo);
 		System.out.println("\nNome: " + listaPF.get(id).nome);
-		System.out.println("\nRenda: " + listaPF.get(id).renda);
-		System.out.println("\nGasto com saude: R$ " + listaPF.get(id).saude);
+		System.out.println("\nRenda: R$ " + listaPF.get(id).renda);
+		System.out.println("\nGasto com saúde: R$ " + listaPF.get(id).saude);
 		System.out.println("\nImposto: R$ " + listaPF.get(id).imposto);	
 	}
 	public static void printPessoaJuridica(List<Juridica> listaPJ, int id)
@@ -52,8 +51,8 @@ public class Aplicacao
 		System.out.println("\n\nContribuinte nº " + id);
 		System.out.println("\nTipo: " + listaPJ.get(id).tipo);
 		System.out.println("\nNome: " + listaPJ.get(id).nome);
-		System.out.println("\nRenda: " + listaPJ.get(id).renda);
-		System.out.println("\nNumero de funcionarios: " + listaPJ.get(id).funcionarios);
+		System.out.println("\nRenda: R$ " + listaPJ.get(id).renda);
+		System.out.println("\nNumero de funcionários: " + listaPJ.get(id).funcionarios);
 		System.out.println("\nImposto: R$ " + listaPJ.get(id).imposto);		
 	}
 	public static boolean seListaVazia(List<Fisica> listaPF, List<Juridica> listaPJ, int opc)
@@ -76,11 +75,22 @@ public class Aplicacao
 			
 			if((id >= listaPF.size() && opc == 1) || (id >= listaPJ.size() && opc == 2))
 			{
-				System.out.println("\n\nIndice fora dos limites da lista! Digite novamente");	
+				System.out.println("\n\nÍndice fora dos limites da lista! Digite novamente");	
 			}
 		}while((id >= listaPF.size() && opc == 1) || (id >= listaPJ.size() && opc == 2) );
 		
 		return id;
+	}
+	public static void printImpostoTotal(int opc, float impostoF, float impostoJ)
+	{
+		if (opc == 1)
+		{
+			System.out.println("\n\nImposto total arrecadado por pessoas físicas: R$ " + impostoF);
+		}
+		else 
+		{
+			System.out.println("\n\nImposto total arrecadado por pessoas Jurídicas: R$ " + impostoJ);
+		}
 	}
 	public static void main(String[] args) 
 	{	 
@@ -98,6 +108,8 @@ public class Aplicacao
 		float imposto;
 		float renda, saude;
 		int funcionarios;
+		float impostoTotalF = 0, impostoTotalJ = 0;
+		float impostoArrecadado;
 		
 		pessoaF = new Fisica(null, null, 0, 0, 0);
 		pessoaJ = new Juridica(null, null, 0, 0, 0);
@@ -110,6 +122,7 @@ public class Aplicacao
 			System.out.println("\n3 - Ver lista de contribuintes  ");
 			System.out.println("\n4 - Remover  contribuinte");
 			System.out.println("\n5 - Deletar toda lista");
+			System.out.println("\n6 - Ver imposto total arrecadado");
 			System.out.println("\n\nResposta: ");
 		
 			op = sc.nextInt();
@@ -118,7 +131,7 @@ public class Aplicacao
 			{
 				case 1:
 					
-					System.out.println("\n\nÉ um contribuinte: \n1 - Pessoa Fisica \n2 - Pessoa Juridica: \n\nResposta: ");
+					System.out.println("\n\nÉ um contribuinte: \n1 - Pessoa Física \n2 - Pessoa Jurídica: \n\nResposta: ");
 					opc = obterTipoPessoa();	
 					
 					System.out.println("\n\nDigite o número de contribuintes a serem inseridos: ");
@@ -135,49 +148,53 @@ public class Aplicacao
 						
 							if(opc == 1) 
 							{
-								System.out.println("\n\nDigite os gastos com saude do contribuinte: ");
+								System.out.println("\n\nDigite os gastos com saúde do contribuinte: ");
 								saude = sc.nextFloat();
 				
-								tipo = new String("Pessoa Fisica");
+								tipo = new String("Pessoa Física");
 								pessoaF.setNome(nome);
 								pessoaF.setRenda(renda);
 								pessoaF.setSaude(saude);
 								pessoaF.setTipo(tipo);
 								imposto = pessoaF.calcularImposto(renda);
 									
-								System.out.println("\n\nImposto = " + imposto);
+								System.out.println("\n\nImposto = R$ " + imposto);
 									
 								Fisica vetorPF [] = new Fisica[N];
 								vetorPF[i] = new Fisica(tipo, nome, renda, imposto, saude);
 								listaPF.add(vetorPF[i]);
+								impostoTotalF += imposto;
 							
 							}
 							else if (opc == 2)
 							{
-								System.out.println("\n\nDigite o numero de funcionarios: ");
+								System.out.println("\n\nDigite o número de funcionários: ");
 								funcionarios = sc.nextInt();
-								tipo = new String("Pessoa Juridica");
+								tipo = new String("Pessoa Jurídica");
 								pessoaJ.setNome(nome);
 								pessoaJ.setRenda(renda);
 								pessoaJ.setFuncionarios(funcionarios);
 								pessoaJ.setTipo(tipo);
 								imposto = pessoaJ.calcularImposto(renda);
 									
-								System.out.println("\n\nImposto = " + imposto);
+								System.out.println("\n\nImposto = R$ " + imposto);
 									
 								Juridica vetorPJ [] = new Juridica[N];
 								vetorPJ[i] = new Juridica(tipo, nome, renda, imposto, funcionarios);
 								listaPJ.add(vetorPJ[i]);
+								impostoTotalJ += imposto;
 							}	
 								
 						}while(opc > 2 || opc < 1);
+						
+						printImpostoTotal(opc, impostoTotalF, impostoTotalJ);
 					}	
 								
 				break;
 					
 				case 2:
 					
-					System.out.println("\n\nÉ um contribuinte: \n1 - Pessoa Fisica \n2 - Pessoa Juridica: \n\nResposta: ");
+					System.out.println("\n\nÉ um contribuinte: \n1 - Pessoa Física \n2 - Pessoa Jurídica: \n\nResposta: ");
 					opc = obterTipoPessoa();	
 					
 					if(seListaVazia( listaPF, listaPJ, opc) == true)
@@ -200,7 +217,7 @@ public class Aplicacao
 				
 				case 3:
 						
-					System.out.println("\n\nDeseja ver a lista de contribuintes: \n1 - Pessoa Fisica \n2 - Pessoa Juridica: \n\nResposta: ");
+					System.out.println("\n\nDeseja ver a lista de contribuintes: \n1 - Pessoa Física \n2 - Pessoa Jurídica: \n\nResposta: ");
 					opc = obterTipoPessoa();	
 					
 					if(seListaVazia( listaPF, listaPJ, opc) == true)
@@ -215,7 +232,7 @@ public class Aplicacao
 							{
 								printPessoaFisica(listaPF, i);
 							}
-						} 
+						}
 						else 
 						{
 							for (i=0; i<listaPJ.size(); i++)
@@ -223,29 +240,32 @@ public class Aplicacao
 								printPessoaJuridica(listaPJ, i);
 							}
 						}
+						printImpostoTotal(opc, impostoTotalF, impostoTotalJ);
 					}
 					
 				break;
 				
 				case 4:
 
-					System.out.println("\n\nDeseja remover um contribuinte: \n1 - Pessoa Fisica \n2 - Pessoa Juridica: \n\nResposta: ");
+					System.out.println("\n\nDeseja remover um contribuinte: \n1 - Pessoa Física \n2 - Pessoa Jurídica: \n\nResposta: ");
 					opc = obterTipoPessoa();
 						
 					if (seListaVazia( listaPF, listaPJ, opc) == true)
 					{
-						System.out.println("\nImpossivel realizar exclusao!");
+						System.out.println("\nImpossível realizar exclusão!");
 					}
 					else 
 					{
 						obterIndicePessoa(listaPF, listaPJ, opc);
 						if (opc == 1)
 						{
+							impostoTotalF -= listaPF.get(id).imposto;
 							listaPF.remove(id);
-							System.out.println("\nRemovido com sucesso! ");
+							System.out.println("\nRemovido com sucesso! ");	
 						} 
 						else 
 						{
+							impostoTotalJ -= listaPJ.get(id).imposto;
 							listaPJ.remove(id);
 							System.out.println("\nRemovido com sucesso! ");
 						}	
@@ -255,24 +275,36 @@ public class Aplicacao
 				
 				case 5:
 					
-					System.out.println("\n\nDeseja deletar a lista de contribuinte: \n1 - Pessoa Fisica \n2 - Pessoa Juridica: \n\nResposta: ");
+					System.out.println("\n\nDeseja deletar a lista de contribuinte: \n1 - Pessoa Física \n2 - Pessoa Jurídica: \n\nResposta: ");
 					opc = obterTipoPessoa();	
 					
 					if (seListaVazia( listaPF, listaPJ, opc) == true)
 					{
-						System.out.println("\nImpossivel realizar exclusao!");
+						System.out.println("\nImpossível realizar exclusão!");
 					}
 					else if (opc == 1)
 					{
+						impostoTotalF = 0;
 						listaPF.removeAll(listaPF);
 						System.out.println("\nRemovido com sucesso! ");
 					}
 					else
 					{
+						impostoTotalJ = 0;
 						listaPJ.removeAll(listaPJ);
 						System.out.println("\nRemovido com sucesso! ");
 					}
 
+				break;
+				
+				case 6:
+					
+						System.out.println("\n\nImpostos arrecadados");
+						System.out.println("\n\nPessoas físicas: R$ " + impostoTotalF);
+						System.out.println("\nPessoas jurídicas: R$ " + impostoTotalJ);
+						impostoArrecadado = impostoTotalF + impostoTotalJ;
+						System.out.println("\n\nImposto total arrecadado: R$ " + impostoArrecadado);
+						
 				break;
 				
 			default:
@@ -281,11 +313,11 @@ public class Aplicacao
 			}	
 			do
 			{
-				System.out.println("\n\nDeseja realizar nova operação?\n1 - SIM; \n2 - NAO\n\nResposta:");
+				System.out.println("\n\nDeseja realizar nova operação?\n1 - SIM; \n2 - NÃO\n\nResposta:");
 				op = sc.nextInt();
 				if (op < 1 || op > 2)
 				{
-					System.out.println("Opcao Invalida! Digite novamente");
+					System.out.println("Opção Inválida! Digite novamente");
 				}
 			}while (op < 1 || op > 2);
 			
