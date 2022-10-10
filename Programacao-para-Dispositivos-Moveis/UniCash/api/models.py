66776ff3d41af5931e60e8eb2deeca0653fa96cf
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-10-10 17:27:25 -0300

    Updated models

--- Programacao-para-Dispositivos-Moveis/UniCash/api/models.py
+++ Programacao-para-Dispositivos-Moveis/UniCash/api/models.py
@@ -3,21 +3,29 @@ from django.db import models
 # Create your models here.
 
 class Users(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    enrollment = models.IntegerField()
-    name = models.CharField(max_length=256)
-    course = models.CharField(max_length=256)
-    image = models.CharField(max_length=256)
+    enrollment = models.CharField(max_length=7, blank=False, null=False)
+    name = models.CharField(max_length=256, blank=False, null=False)
+    course = models.CharField(max_length=256, blank=False, null=False)
+    image = models.URLField(max_length=256, blank=True, null=True)
 
 class Wallet(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    balance = models.IntegerField()
+    user_id = models.ForeignKey(
+        Users, 
+        on_delete=models.CASCADE, 
+        blank=False, 
+        null=False
+    )
+    balance = models.IntegerField(default=0, editable=True)
 
 class Receipt(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    transfer_value = models.IntegerField()
-    wallet_id = models.ForeignKey(Wallet, on_delete=models.CASCADE)
-    transfer_user = models.IntegerField()
+    transfer_value = models.IntegerField(blank=False, null=False)
+    wallet_id = models.ForeignKey(
+        Wallet, 
+        on_delete=models.CASCADE, 
+        blank=False, 
+        null=False,
+        related_name="receipts"
+    )
+    transfer_user = models.CharField(max_length=256, blank=False, null=False)
     date = models.DateTimeField(auto_now=True)
 
