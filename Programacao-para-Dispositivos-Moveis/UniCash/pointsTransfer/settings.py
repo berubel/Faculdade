fd972bf2d53a03179d73d8cdcf2ec902b549d7fc
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-10-18 02:30:27 -0300

    Migrate database to PostgreSQL

--- Programacao-para-Dispositivos-Moveis/UniCash/pointsTransfer/settings.py
+++ Programacao-para-Dispositivos-Moveis/UniCash/pointsTransfer/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'pointsTransfer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pointsTransfer',
+        'USER': 'postgres',
+        'PASSWORD': '22022002',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
